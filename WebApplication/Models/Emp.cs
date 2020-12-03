3c896603de319ad88bf37cf69c821c7a2af8f91e
Prashant097 <prashanttrivedi097@gmail.com>
2020-12-03 15:57:58 +0530

    Added Properties

---
+++ WebApplication/Models/Emp.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace WebApplication.Models
+{
+    public class Emp
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Desig { get; set; }
+        public DateTime DOJ { get; set; }
+
+    }
+}
